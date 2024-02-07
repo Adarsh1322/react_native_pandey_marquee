a977d4673e81df213a5573576e6f8fba2f918983
AdarshShine <adarsh.pandey@shinedezign.com>
2024-02-07 18:33:37 +0530

    new changes

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 
 import React from 'react'
 import { Text } from 'react-native';
+import Marquee from 'react_native_pandey_marquee';
+
 const App = () => {
   return (
     <Marquee spacing={0} speed={0.7} direction='left'autofill={false} >
