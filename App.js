24e2b9333c288cd36ecc7b373d1d82bf7ad495f6
AdarshShine <adarsh.pandey@shinedezign.com>
2024-02-13 18:31:52 +0530

    New Changes

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import Marquee from 'react_native_pandey_marquee';
 
 const App = () => {
   return (
-    <Marquee spacing={0} speed={0.7} direction='left'autofill={false} >
-      <Text style={{marginTop:60,fontSize:16, color:'red',}}> world Hello pandey</Text>
+    <Marquee spacing={0} speed={0.7} direction='left'autofill={false} linearGradientColors={['#FF5733', '#FFC300']} >
+      <Text style={{marginTop:60,fontSize:16, color:'red',}}> Hello World </Text>
     </Marquee>
   )
 }
